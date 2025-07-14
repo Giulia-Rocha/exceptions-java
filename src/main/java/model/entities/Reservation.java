449e4892c5a6a431270f414c3c447fb10f8f6fbd
Giulia Barbizan <giuliabarbizan@gmail.com>
2025-07-14 11:22:13 -0300

    first commit

---
+++ src/main/java/model/entities/Reservation.java
@@ -0,0 +1,19 @@
+package model.entities;
+
+import java.util.Date;
+
+public class Reservation {
+
+    private int roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+
+    public Integer duration(){
+
+    }
+
+    public void updateDates(Date checkIn, Date checkOut){
+
+    }
+}
